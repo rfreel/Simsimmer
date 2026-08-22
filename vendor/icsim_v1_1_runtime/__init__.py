036df0a00b2d118da3ed39374215768324b37fba
rfreel <ryanfreel@gmail.com>
2026-08-22 07:42:25 -0700

    vendor: add v1.1 runtime package shim

---
+++ vendor/icsim_v1_1_runtime/__init__.py
@@ -0,0 +1,5 @@
+from .engine import Rival, Runtime
+from .model import ClaimState, Event, Projection, Trace
+
+__all__ = ["Runtime", "Rival", "Event", "Projection", "Trace", "ClaimState"]
+__version__ = "1.1.0-runtime-subset"
